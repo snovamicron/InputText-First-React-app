2fe526a53b2fa4269f9d618c9030838a3a963ed4
snovamicron <mdip5675@gmail.com>
2021-11-22 21:29:37 +0530

    modified little more

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,12 +7,12 @@ const NavBar = (prop) => {
     <>
       <nav className={`navbar navbar-expand-lg navbar-dark bg-${ Mode === 'dark'?'secondary':'dark'}`}>
         <div className="container-fluid">
-          <a className="navbar-brand"  href= "/#">{ webTitle} </a>
+          <a className="navbar-brand"  href= "#">{ webTitle} </a>
           {/* <Link className="navbar-brand"  to= "/ ">{ webTitle} </Link> */}
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-              <a className="nav-link active" aria-current="page"  href="#/">{home} </a>
+              <a className="nav-link active" aria-current="page"  href="#">{home} </a>
               {/* <Link className="nav-link active" aria-current="page"  to="/">{home} </Link> */}
               </li>
               <li className="nav-item">
